273426255bcda7ed1d18dc9d676c75d687eece44
Pixelated <toxic.dev09@gmail.com>
2026-03-29 12:26:46 -0600

    Update body size limit and proxy client max body size

--- next.config.ts
+++ next.config.ts
@@ -24,8 +24,9 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   experimental: {
+    proxyClientMaxBodySize: '2gb',
     serverActions: {
-      bodySizeLimit: '5gb',
+      bodySizeLimit: '2gb',
     },
 
     optimizePackageImports: [
